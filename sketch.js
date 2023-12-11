fa2bb61ea1e90da32e4310a30a61d1d08806f273
brycedli <bryce.d.li@gmail.com>
2023-12-11 14:30:05 -0500

    initial VHS

--- sketch.js
+++ sketch.js
@@ -1,16 +1,42 @@
+let video;
+let main;
+let vhs;
+let noiseGraphics;
 function preload() {
-	// put preload code here
+  vhs = loadShader("assets/shaders/vhs.vert", "assets/shaders/vhs.frag");
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template repository\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  video = createVideo("assets/videos/bear.mp4");
+  video.hide();
+  noiseGraphics = createGraphics(windowWidth, windowHeight);
+	main = createGraphics(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight, WEBGL);
+	
 }
 
 function draw() {
-	// put drawing code here
+  main.image(video, 0, 0, width, height);
+  noiseGraphics.background(0);
+  noiseGraphics.noStroke();
+  let n = 240;
+  let noiseHeight = windowHeight/n;
+  for (let i = 0; i < n; i++) {
+    
+    noiseGraphics.fill(random(80));
+    noiseGraphics.rect(0, i*noiseHeight, windowWidth, noiseHeight);
+  }
+  vhs.setUniform('canvas', main);
+  vhs.setUniform("noiseCanvas", noiseGraphics);
+  vhs.setUniform("uResolution", [windowWidth, windowHeight]);
+  vhs.setUniform("uMouse", [mouseX,mouseY]);
+  vhs.setUniform("uTime", millis()/1000);
+  shader(vhs);
+  rotateY(PI/2);
+  rect(0, 0, width, height);
+	// image(main, 0, 0, width, height);
+}
+
+function mousePressed(){
+  video.loop();
 }
